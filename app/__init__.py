c5bcd50ca08a9965f4f7530f3f18fc02da468788
Peter Mankiewich <pmank64@gmail.com>
2018-12-02 14:03:39 -0500

    Login Stuff

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,15 @@
 from flask import Flask
-
+# from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes
