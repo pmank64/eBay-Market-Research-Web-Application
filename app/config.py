a2f287de235bd8a3fb8b4d35fc6c13154190d935
ccantoral2121 <cristianggerardo@hotmail.com>
2018-12-15 20:00:24 -0500

    deployment

--- app/config.py
+++ app/config.py
@@ -7,3 +7,8 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class Config(object):
+        # ...
+     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
